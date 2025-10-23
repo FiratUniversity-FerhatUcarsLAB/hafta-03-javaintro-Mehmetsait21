25bb29b1c6a7dc72d361ff0b1f82548d18b4fa08
Mehmet Sait TAYŞİ <250541074@firat.edu.tr>
2025-10-23 13:15:47 +0300

    Add welcome messages in UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hosgeldiniz");
+        System.out.println("Bilgisayar Bilimine Hos Geldiniz");
+        System.out.println("Programlama E˘glencelidir");
+    }
+}
