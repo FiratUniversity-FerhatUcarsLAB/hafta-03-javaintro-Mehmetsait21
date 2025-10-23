e0f61a893e203fe5cb426036dcdebb76b2ada9f0
Mehmet Sait TAYŞİ <250541074@firat.edu.tr>
2025-10-23 14:42:46 +0300

    Add multiplication table for number 2

--- Task-5-TabloDesenleri/CarpimTablosu.java
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -1,2 +1,15 @@
+public class CarpimTablosu2 {
+    public static void main(String[] args) {
+        int sayi = 2;
+
+        System.out.println(sayi + " sayısının çarpım tablosu:");
+        System.out.println("---------------------------");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(sayi + " x " + i + " = " + (sayi * i));
+        }
+    }
+}
+
 
 
