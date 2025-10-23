7c113dc01b502c265f881823c4f8f7857843275f
Mehmet Sait TAYŞİ <250541074@firat.edu.tr>
2025-10-23 13:37:06 +0300

    Implement sum calculation from 1 to 9
    Calculate the sum of numbers from 1 to 9 and print the result.

---
+++ Task-2-Matematikselislemler/Faktoriyel.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+       // Sonucu hesapla
+ int sonuc = 1+2+3+4+5+6+7+8+9;
+
+ // Ekrana yazdir
+ System . out . println (" 1+2+...+9 = " + sonuc ) ;
+    }
+}
